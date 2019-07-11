d81a86ef4b92ecdc418e8756d56c280c1750933d
AnkeKing <2678924540@qq.com>
2019-07-11 10:27:00 +0800

    init `

---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+      <h1>登录</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Box",
+  data() {
+    return {};
+  },
+  components: {}
+};
+</script>
+
+<style  rel='stylesheet/scss' lang='scss' scoped>
+</style>
